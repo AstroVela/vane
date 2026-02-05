evertlammerts/duckdb-python

Add arrow_table() and arrow_reader() and deprecate old api

tests/fast/api/test_streaming_result.py
@@ -41,7 +41,7 @@ class TestStreamingResult:
         pytest.importorskip("pyarrow.dataset")
         # record batch reader
         res = duckdb_cursor.sql("SELECT * FROM range(100000) t(i)")
-        reader = res.fetch_arrow_reader(batch_size=16_384)
+        reader = res.arrow_reader(batch_size=16_384)
         result = []
         for batch in reader:
             result += batch.to_pydict()["i"]
@@ -52,7 +52,7 @@ class TestStreamingResult:
             "SELECT CASE WHEN i < 10000 THEN i ELSE concat('hello', i::VARCHAR)::INT END FROM range(100000) t(i)"
         )
         with pytest.raises(duckdb.ConversionException, match="Could not convert string 'hello10000' to INT32"):
-            reader = res.fetch_arrow_reader(batch_size=16_384)
+            reader = res.arrow_reader(batch_size=16_384)
 
     def test_9801(self, duckdb_cursor):
         duckdb_cursor.execute("CREATE TABLE test(id INTEGER , name VARCHAR NOT NULL);")

tests/fast/arrow/test_arrow_binary_view.py
@@ -11,10 +11,10 @@ class TestArrowBinaryView:
         tab = pa.table({"x": pa.array([b"abc", b"thisisaverybigbinaryyaymorethanfifteen", None], pa.binary_view())})
         assert con.execute("FROM tab").fetchall() == [(b"abc",), (b"thisisaverybigbinaryyaymorethanfifteen",), (None,)]
         # By default we won't export a view
-        assert not con.execute("FROM tab").fetch_arrow_table().equals(tab)
+        assert not con.execute("FROM tab").arrow_table().equals(tab)
         # We do the binary view from 1.4 onwards
         con.execute("SET arrow_output_version = 1.4")
-        assert con.execute("FROM tab").fetch_arrow_table().equals(tab)
+        assert con.execute("FROM tab").arrow_table().equals(tab)
 
         assert con.execute("FROM tab where x = 'thisisaverybigbinaryyaymorethanfifteen'").fetchall() == [
             (b"thisisaverybigbinaryyaymorethanfifteen",)

tests/fast/arrow/test_arrow_decimal256.py
@@ -17,4 +17,4 @@ class TestArrowDecimal256:
             with pytest.raises(
                 duckdb.NotImplementedException, match="Unsupported Internal Arrow Type for Decimal d:12,4,256"
             ):
-                conn.execute("select * from pa_decimal256;").fetch_arrow_table().to_pylist()
+                conn.execute("select * from pa_decimal256;").arrow_table().to_pylist()

tests/fast/arrow/test_arrow_decimal_32_64.py
@@ -33,7 +33,7 @@ class TestArrowDecimalTypes:
         ]
 
         # Test write
-        arrow_table = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table()
+        arrow_table = duckdb_cursor.execute("FROM decimal_32").arrow_table()
 
         assert arrow_table.equals(decimal_32)
 
@@ -64,5 +64,5 @@ class TestArrowDecimalTypes:
         ).fetchall() == [(2,)]
 
         # Test write
-        arrow_table = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table()
+        arrow_table = duckdb_cursor.execute("FROM decimal_64").arrow_table()
         assert arrow_table.equals(decimal_64)

tests/fast/arrow/test_arrow_fetch.py
@@ -14,7 +14,7 @@ except Exception:
 def check_equal(duckdb_conn):
     true_result = duckdb_conn.execute("SELECT * from test").fetchall()
     duck_tbl = duckdb_conn.table("test")
-    duck_from_arrow = duckdb_conn.from_arrow(duck_tbl.fetch_arrow_table())
+    duck_from_arrow = duckdb_conn.from_arrow(duck_tbl.arrow_table())
     duck_from_arrow.create("testarrow")
     arrow_result = duckdb_conn.execute("SELECT * from testarrow").fetchall()
     assert arrow_result == true_result
@@ -86,7 +86,7 @@ class TestArrowFetch:
         duckdb_cursor = duckdb.connect()
         duckdb_cursor.execute("CREATE table t as select range a from range(3000);")
         relation = duckdb_cursor.table("t")
-        arrow_tbl = relation.fetch_arrow_table()
+        arrow_tbl = relation.arrow_table()
         assert arrow_tbl["a"].num_chunks == 1
-        arrow_tbl = relation.fetch_arrow_table(2048)
+        arrow_tbl = relation.arrow_table(2048)
         assert arrow_tbl["a"].num_chunks == 2

tests/fast/arrow/test_arrow_version_format.py
@@ -20,7 +20,7 @@ class TestArrowDecimalTypes:
             ],
             pa.schema([("data", pa.decimal32(5, 2))]),
         )
-        col_type = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("FROM decimal_32").arrow_table().schema.field("data").type
         assert col_type.bit_width == 32
         assert pa.types.is_decimal(col_type)
 
@@ -33,12 +33,12 @@ class TestArrowDecimalTypes:
             ],
             pa.schema([("data", pa.decimal64(16, 3))]),
         )
-        col_type = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("FROM decimal_64").arrow_table().schema.field("data").type
         assert col_type.bit_width == 64
         assert pa.types.is_decimal(col_type)
         for version in ["1.0", "1.1", "1.2", "1.3", "1.4"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            result = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table()
+            result = duckdb_cursor.execute("FROM decimal_32").arrow_table()
             col_type = result.schema.field("data").type
             assert col_type.bit_width == 128
             assert pa.types.is_decimal(col_type)
@@ -46,7 +46,7 @@ class TestArrowDecimalTypes:
                 "data": [Decimal("100.20"), Decimal("110.21"), Decimal("31.20"), Decimal("500.20")]
             }
 
-            result = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table()
+            result = duckdb_cursor.execute("FROM decimal_64").arrow_table()
             col_type = result.schema.field("data").type
             assert col_type.bit_width == 128
             assert pa.types.is_decimal(col_type)
@@ -64,31 +64,31 @@ class TestArrowDecimalTypes:
         duckdb_cursor.execute("SET arrow_output_version = 1.5")
         duckdb_cursor.execute("SET produce_arrow_string_view=True")
         duckdb_cursor.execute("SET arrow_output_list_view=True")
-        col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("SELECT 'string' as data ").arrow_table().schema.field("data").type
         assert pa.types.is_string_view(col_type)
-        col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("SELECT ['string'] as data ").arrow_table().schema.field("data").type
         assert pa.types.is_list_view(col_type)
 
         for version in ["1.0", "1.1", "1.2", "1.3"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").arrow_table().schema.field("data").type
             assert not pa.types.is_string_view(col_type)
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").arrow_table().schema.field("data").type
             assert not pa.types.is_list_view(col_type)
 
         for version in ["1.4", "1.5"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").arrow_table().schema.field("data").type
             assert pa.types.is_string_view(col_type)
 
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").arrow_table().schema.field("data").type
             assert pa.types.is_list_view(col_type)
 
         duckdb_cursor.execute("SET produce_arrow_string_view=False")
         duckdb_cursor.execute("SET arrow_output_list_view=False")
         for version in ["1.4", "1.5"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").arrow_table().schema.field("data").type
             assert not pa.types.is_string_view(col_type)
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").arrow_table().schema.field("data").type
             assert not pa.types.is_list_view(col_type)

tests/fast/arrow/test_filter_pushdown.py
@@ -24,7 +24,7 @@ def create_pyarrow_pandas(rel):
 
 
 def create_pyarrow_table(rel):
-    return rel.fetch_arrow_table()
+    return rel.arrow_table()
 
 
 def create_pyarrow_dataset(rel):
@@ -553,7 +553,7 @@ class TestArrowFilterPushdown:
         df.to_parquet(str(file_path))
 
         my_arrow_dataset = pa_ds.dataset(str(file_path))
-        res = duckdb_cursor.execute("SELECT * FROM my_arrow_dataset WHERE ts = ?", parameters=[dt]).fetch_arrow_table()
+        res = duckdb_cursor.execute("SELECT * FROM my_arrow_dataset WHERE ts = ?", parameters=[dt]).arrow_table()
         output = duckdb_cursor.sql("select * from res").fetchall()
         expected = [(1, dt), (2, dt), (3, dt)]
         assert output == expected
@@ -704,7 +704,7 @@ class TestArrowFilterPushdown:
         duckdb_cursor.execute(f"copy (select * from df2) to '{data2.as_posix()}'")
 
         glob_pattern = tmp_path / "data*.parquet"
-        table = duckdb_cursor.read_parquet(glob_pattern.as_posix()).fetch_arrow_table()
+        table = duckdb_cursor.read_parquet(glob_pattern.as_posix()).arrow_table()
 
         output_df = duckdb.arrow(table).filter("date > '2019-01-01'").df()
         expected_df = duckdb.from_parquet(glob_pattern.as_posix()).filter("date > '2019-01-01'").df()
@@ -869,7 +869,7 @@ class TestArrowFilterPushdown:
         con.execute(
             "CREATE TABLE T as SELECT i::integer a, i::varchar b, i::uhugeint c, i::integer d FROM range(5) tbl(i)"
         )
-        arrow_tbl = con.execute("FROM T").fetch_arrow_table()
+        arrow_tbl = con.execute("FROM T").arrow_table()
 
         # No projection just unsupported filter
         assert con.execute("from arrow_tbl where c == 3").fetchall() == [(3, "3", 3, 3)]
@@ -893,7 +893,7 @@ class TestArrowFilterPushdown:
             "CREATE TABLE T_2 as SELECT i::integer a, i::varchar b, i::uhugeint c, i::integer d , i::uhugeint e, i::smallint f, i::uhugeint g FROM range(50) tbl(i)"  # noqa: E501
         )
 
-        arrow_tbl = con.execute("FROM T_2").fetch_arrow_table()
+        arrow_tbl = con.execute("FROM T_2").arrow_table()
 
         assert con.execute(
             "select a, b from arrow_tbl where a > 2 and c < 40 and b == '28' and g > 15 and e < 30"
@@ -905,8 +905,8 @@ class TestArrowFilterPushdown:
         duckdb_conn.execute("CREATE TABLE build as select (random()*9999)::INT b from range(20);")
         duck_probe = duckdb_conn.table("probe")
         duck_build = duckdb_conn.table("build")
-        duck_probe_arrow = duck_probe.fetch_arrow_table()
-        duck_build_arrow = duck_build.fetch_arrow_table()
+        duck_probe_arrow = duck_probe.arrow_table()
+        duck_build_arrow = duck_build.arrow_table()
         duckdb_conn.register("duck_probe_arrow", duck_probe_arrow)
         duckdb_conn.register("duck_build_arrow", duck_build_arrow)
         assert duckdb_conn.execute("SELECT count(*) from duck_probe_arrow, duck_build_arrow where a=b").fetchall() == [
@@ -917,7 +917,7 @@ class TestArrowFilterPushdown:
         duckdb_conn = duckdb.connect()
         duckdb_conn.execute("CREATE TABLE probe as select range a from range(1000);")
         duck_probe = duckdb_conn.table("probe")
-        duck_probe_arrow = duck_probe.fetch_arrow_table()
+        duck_probe_arrow = duck_probe.arrow_table()
         duckdb_conn.register("duck_probe_arrow", duck_probe_arrow)
         assert duckdb_conn.execute("SELECT * from duck_probe_arrow where a = any([1,999])").fetchall() == [(1,), (999,)]
 
@@ -980,7 +980,7 @@ class TestArrowFilterPushdown:
             arrow_res = con.sql(query.format(table="arrow_table")).fetchall()
             assert len(duckdb_res) == len(arrow_res)
 
-        arrow_table = duckdb_cursor.table("test").fetch_arrow_table()
+        arrow_table = duckdb_cursor.table("test").arrow_table()
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a > 'NaN'::FLOAT")
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a >= 'NaN'::FLOAT")
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a < 'NaN'::FLOAT")

AstroVela/vane

tests/fast/api/test_streaming_result.py
@@ -41,7 +41,7 @@ class TestStreamingResult:
         pytest.importorskip("pyarrow.dataset")
         # record batch reader
         res = duckdb_cursor.sql("SELECT * FROM range(100000) t(i)")
-        reader = res.fetch_arrow_reader(batch_size=16_384)
+        reader = res.to_arrow_reader(batch_size=16_384)
         result = []
         for batch in reader:
             result += batch.to_pydict()["i"]
@@ -52,7 +52,7 @@ class TestStreamingResult:
             "SELECT CASE WHEN i < 10000 THEN i ELSE concat('hello', i::VARCHAR)::INT END FROM range(100000) t(i)"
         )
         with pytest.raises(duckdb.ConversionException, match="Could not convert string 'hello10000' to INT32"):
-            reader = res.fetch_arrow_reader(batch_size=16_384)
+            reader = res.to_arrow_reader(batch_size=16_384)
 
     def test_9801(self, duckdb_cursor):
         duckdb_cursor.execute("CREATE TABLE test(id INTEGER , name VARCHAR NOT NULL);")

tests/fast/arrow/test_arrow_binary_view.py
@@ -11,10 +11,10 @@ class TestArrowBinaryView:
         tab = pa.table({"x": pa.array([b"abc", b"thisisaverybigbinaryyaymorethanfifteen", None], pa.binary_view())})
         assert con.execute("FROM tab").fetchall() == [(b"abc",), (b"thisisaverybigbinaryyaymorethanfifteen",), (None,)]
         # By default we won't export a view
-        assert not con.execute("FROM tab").fetch_arrow_table().equals(tab)
+        assert not con.execute("FROM tab").to_arrow_table().equals(tab)
         # We do the binary view from 1.4 onwards
         con.execute("SET arrow_output_version = 1.4")
-        assert con.execute("FROM tab").fetch_arrow_table().equals(tab)
+        assert con.execute("FROM tab").to_arrow_table().equals(tab)
 
         assert con.execute("FROM tab where x = 'thisisaverybigbinaryyaymorethanfifteen'").fetchall() == [
             (b"thisisaverybigbinaryyaymorethanfifteen",)

tests/fast/arrow/test_arrow_decimal256.py
@@ -17,4 +17,4 @@ class TestArrowDecimal256:
             with pytest.raises(
                 duckdb.NotImplementedException, match="Unsupported Internal Arrow Type for Decimal d:12,4,256"
             ):
-                conn.execute("select * from pa_decimal256;").fetch_arrow_table().to_pylist()
+                conn.execute("select * from pa_decimal256;").to_arrow_table().to_pylist()

tests/fast/arrow/test_arrow_decimal_32_64.py
@@ -33,7 +33,7 @@ class TestArrowDecimalTypes:
         ]
 
         # Test write
-        arrow_table = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table()
+        arrow_table = duckdb_cursor.execute("FROM decimal_32").to_arrow_table()
 
         assert arrow_table.equals(decimal_32)
 
@@ -64,5 +64,5 @@ class TestArrowDecimalTypes:
         ).fetchall() == [(2,)]
 
         # Test write
-        arrow_table = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table()
+        arrow_table = duckdb_cursor.execute("FROM decimal_64").to_arrow_table()
         assert arrow_table.equals(decimal_64)

tests/fast/arrow/test_arrow_fetch.py
@@ -14,7 +14,7 @@ except Exception:
 def check_equal(duckdb_conn):
     true_result = duckdb_conn.execute("SELECT * from test").fetchall()
     duck_tbl = duckdb_conn.table("test")
-    duck_from_arrow = duckdb_conn.from_arrow(duck_tbl.fetch_arrow_table())
+    duck_from_arrow = duckdb_conn.from_arrow(duck_tbl.to_arrow_table())
     duck_from_arrow.create("testarrow")
     arrow_result = duckdb_conn.execute("SELECT * from testarrow").fetchall()
     assert arrow_result == true_result
@@ -86,7 +86,7 @@ class TestArrowFetch:
         duckdb_cursor = duckdb.connect()
         duckdb_cursor.execute("CREATE table t as select range a from range(3000);")
         relation = duckdb_cursor.table("t")
-        arrow_tbl = relation.fetch_arrow_table()
+        arrow_tbl = relation.to_arrow_table()
         assert arrow_tbl["a"].num_chunks == 1
-        arrow_tbl = relation.fetch_arrow_table(2048)
+        arrow_tbl = relation.to_arrow_table(2048)
         assert arrow_tbl["a"].num_chunks == 2

tests/fast/arrow/test_arrow_version_format.py
@@ -20,7 +20,7 @@ class TestArrowDecimalTypes:
             ],
             pa.schema([("data", pa.decimal32(5, 2))]),
         )
-        col_type = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("FROM decimal_32").to_arrow_table().schema.field("data").type
         assert col_type.bit_width == 32
         assert pa.types.is_decimal(col_type)
 
@@ -33,12 +33,12 @@ class TestArrowDecimalTypes:
             ],
             pa.schema([("data", pa.decimal64(16, 3))]),
         )
-        col_type = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("FROM decimal_64").to_arrow_table().schema.field("data").type
         assert col_type.bit_width == 64
         assert pa.types.is_decimal(col_type)
         for version in ["1.0", "1.1", "1.2", "1.3", "1.4"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            result = duckdb_cursor.execute("FROM decimal_32").fetch_arrow_table()
+            result = duckdb_cursor.execute("FROM decimal_32").to_arrow_table()
             col_type = result.schema.field("data").type
             assert col_type.bit_width == 128
             assert pa.types.is_decimal(col_type)
@@ -46,7 +46,7 @@ class TestArrowDecimalTypes:
                 "data": [Decimal("100.20"), Decimal("110.21"), Decimal("31.20"), Decimal("500.20")]
             }
 
-            result = duckdb_cursor.execute("FROM decimal_64").fetch_arrow_table()
+            result = duckdb_cursor.execute("FROM decimal_64").to_arrow_table()
             col_type = result.schema.field("data").type
             assert col_type.bit_width == 128
             assert pa.types.is_decimal(col_type)
@@ -64,31 +64,31 @@ class TestArrowDecimalTypes:
         duckdb_cursor.execute("SET arrow_output_version = 1.5")
         duckdb_cursor.execute("SET produce_arrow_string_view=True")
         duckdb_cursor.execute("SET arrow_output_list_view=True")
-        col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("SELECT 'string' as data ").to_arrow_table().schema.field("data").type
         assert pa.types.is_string_view(col_type)
-        col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+        col_type = duckdb_cursor.execute("SELECT ['string'] as data ").to_arrow_table().schema.field("data").type
         assert pa.types.is_list_view(col_type)
 
         for version in ["1.0", "1.1", "1.2", "1.3"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").to_arrow_table().schema.field("data").type
             assert not pa.types.is_string_view(col_type)
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").to_arrow_table().schema.field("data").type
             assert not pa.types.is_list_view(col_type)
 
         for version in ["1.4", "1.5"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").to_arrow_table().schema.field("data").type
             assert pa.types.is_string_view(col_type)
 
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").to_arrow_table().schema.field("data").type
             assert pa.types.is_list_view(col_type)
 
         duckdb_cursor.execute("SET produce_arrow_string_view=False")
         duckdb_cursor.execute("SET arrow_output_list_view=False")
         for version in ["1.4", "1.5"]:
             duckdb_cursor.execute(f"SET arrow_output_version = {version}")
-            col_type = duckdb_cursor.execute("SELECT 'string' as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT 'string' as data ").to_arrow_table().schema.field("data").type
             assert not pa.types.is_string_view(col_type)
-            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").fetch_arrow_table().schema.field("data").type
+            col_type = duckdb_cursor.execute("SELECT ['string'] as data ").to_arrow_table().schema.field("data").type
             assert not pa.types.is_list_view(col_type)

tests/fast/arrow/test_filter_pushdown.py
@@ -24,7 +24,7 @@ def create_pyarrow_pandas(rel):
 
 
 def create_pyarrow_table(rel):
-    return rel.fetch_arrow_table()
+    return rel.to_arrow_table()
 
 
 def create_pyarrow_dataset(rel):
@@ -553,7 +553,7 @@ class TestArrowFilterPushdown:
         df.to_parquet(str(file_path))
 
         my_arrow_dataset = pa_ds.dataset(str(file_path))
-        res = duckdb_cursor.execute("SELECT * FROM my_arrow_dataset WHERE ts = ?", parameters=[dt]).fetch_arrow_table()
+        res = duckdb_cursor.execute("SELECT * FROM my_arrow_dataset WHERE ts = ?", parameters=[dt]).to_arrow_table()
         output = duckdb_cursor.sql("select * from res").fetchall()
         expected = [(1, dt), (2, dt), (3, dt)]
         assert output == expected
@@ -704,7 +704,7 @@ class TestArrowFilterPushdown:
         duckdb_cursor.execute(f"copy (select * from df2) to '{data2.as_posix()}'")
 
         glob_pattern = tmp_path / "data*.parquet"
-        table = duckdb_cursor.read_parquet(glob_pattern.as_posix()).fetch_arrow_table()
+        table = duckdb_cursor.read_parquet(glob_pattern.as_posix()).to_arrow_table()
 
         output_df = duckdb.arrow(table).filter("date > '2019-01-01'").df()
         expected_df = duckdb.from_parquet(glob_pattern.as_posix()).filter("date > '2019-01-01'").df()
@@ -869,7 +869,7 @@ class TestArrowFilterPushdown:
         con.execute(
             "CREATE TABLE T as SELECT i::integer a, i::varchar b, i::uhugeint c, i::integer d FROM range(5) tbl(i)"
         )
-        arrow_tbl = con.execute("FROM T").fetch_arrow_table()
+        arrow_tbl = con.execute("FROM T").to_arrow_table()
 
         # No projection just unsupported filter
         assert con.execute("from arrow_tbl where c == 3").fetchall() == [(3, "3", 3, 3)]
@@ -893,7 +893,7 @@ class TestArrowFilterPushdown:
             "CREATE TABLE T_2 as SELECT i::integer a, i::varchar b, i::uhugeint c, i::integer d , i::uhugeint e, i::smallint f, i::uhugeint g FROM range(50) tbl(i)"  # noqa: E501
         )
 
-        arrow_tbl = con.execute("FROM T_2").fetch_arrow_table()
+        arrow_tbl = con.execute("FROM T_2").to_arrow_table()
 
         assert con.execute(
             "select a, b from arrow_tbl where a > 2 and c < 40 and b == '28' and g > 15 and e < 30"
@@ -905,8 +905,8 @@ class TestArrowFilterPushdown:
         duckdb_conn.execute("CREATE TABLE build as select (random()*9999)::INT b from range(20);")
         duck_probe = duckdb_conn.table("probe")
         duck_build = duckdb_conn.table("build")
-        duck_probe_arrow = duck_probe.fetch_arrow_table()
-        duck_build_arrow = duck_build.fetch_arrow_table()
+        duck_probe_arrow = duck_probe.to_arrow_table()
+        duck_build_arrow = duck_build.to_arrow_table()
         duckdb_conn.register("duck_probe_arrow", duck_probe_arrow)
         duckdb_conn.register("duck_build_arrow", duck_build_arrow)
         assert duckdb_conn.execute("SELECT count(*) from duck_probe_arrow, duck_build_arrow where a=b").fetchall() == [
@@ -917,7 +917,7 @@ class TestArrowFilterPushdown:
         duckdb_conn = duckdb.connect()
         duckdb_conn.execute("CREATE TABLE probe as select range a from range(1000);")
         duck_probe = duckdb_conn.table("probe")
-        duck_probe_arrow = duck_probe.fetch_arrow_table()
+        duck_probe_arrow = duck_probe.to_arrow_table()
         duckdb_conn.register("duck_probe_arrow", duck_probe_arrow)
         assert duckdb_conn.execute("SELECT * from duck_probe_arrow where a = any([1,999])").fetchall() == [(1,), (999,)]
 
@@ -980,7 +980,7 @@ class TestArrowFilterPushdown:
             arrow_res = con.sql(query.format(table="arrow_table")).fetchall()
             assert len(duckdb_res) == len(arrow_res)
 
-        arrow_table = duckdb_cursor.table("test").fetch_arrow_table()
+        arrow_table = duckdb_cursor.table("test").to_arrow_table()
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a > 'NaN'::FLOAT")
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a >= 'NaN'::FLOAT")
         assert_equal_results(duckdb_cursor, arrow_table, "select * from {table} where a < 'NaN'::FLOAT")