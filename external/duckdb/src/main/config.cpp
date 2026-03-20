746e2f22b826d581b3c994ce8c3c437ef839822a
Mark <mark.raasveldt@gmail.com>
2026-03-20 12:52:52 +0100

    Make some MultiStatements and PRAGMAs Transactional (duckdb/duckdb#21171)
    https://github.com/duckdblabs/duckdb-internal/issues/7551
    
    Make multi-statements transactional so that if one sub-statement fails,
    all previous sub-statements are rolled back.
    
    Same for PRAGMAs, as `COPY FROM DATABASE ...` is a PRAGMA, decomposed
    into
    1. Copy the Schema
    2. Copy the Data
    
    Which is now transactional as a whole.
    
    Upstream-Commit: https://github.com/duckdb/duckdb/commit/6ff3b4f26cc9a916896f89167922704296ef9b6a

--- external/duckdb/src/main/config.cpp
+++ external/duckdb/src/main/config.cpp
@@ -96,6 +96,7 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_GLOBAL(BlockAllocatorMemorySetting),
     DUCKDB_SETTING(CatalogErrorMaxSchemasSetting),
     DUCKDB_GLOBAL(CheckpointThresholdSetting),
+    DUCKDB_SETTING_CALLBACK(CurrentTransactionInvalidationPolicySetting),
     DUCKDB_SETTING(CustomExtensionRepositorySetting),
     DUCKDB_LOCAL(CustomProfilingSettingsSetting),
     DUCKDB_GLOBAL(CustomUserAgentSetting),
@@ -215,12 +216,12 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_SETTING(ZstdMinStringLengthSetting),
     FINAL_SETTING};
 
-static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 101),
-                                                     DUCKDB_SETTING_ALIAS("null_order", 41),
-                                                     DUCKDB_SETTING_ALIAS("profiling_output", 120),
-                                                     DUCKDB_SETTING_ALIAS("user", 135),
+static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 102),
+                                                     DUCKDB_SETTING_ALIAS("null_order", 42),
+                                                     DUCKDB_SETTING_ALIAS("profiling_output", 121),
+                                                     DUCKDB_SETTING_ALIAS("user", 136),
                                                      DUCKDB_SETTING_ALIAS("wal_autocheckpoint", 24),
-                                                     DUCKDB_SETTING_ALIAS("worker_threads", 134),
+                                                     DUCKDB_SETTING_ALIAS("worker_threads", 135),
                                                      FINAL_ALIAS};
 
 vector<ConfigurationOption> DBConfig::GetOptions() {
