59ba29b73120ecc8cd9c4754c119c326dac53493
Mark <mark.raasveldt@gmail.com>
2026-04-01 10:46:18 +0200

    Fix update plans when deserializing if type no longer supports regular updates (duckdb/duckdb#21718)
    This PR is a followup to #21641 which attempts to also add
    backwards-compatible deserialization when reading plans serialized from
    older DuckDB versions that supported in-place updates of geometry
    columns.
    
    This is done by traversing the deserialized plan in the direction of the
    last bound column ref (which is always the `rowid` for LogicalUpdate
    operators), collecting all the projections along the way until the
    source table scan is found. We then add column references for the
    remaining columns in the table to all projections bottom-up and finally
    switching the `update_is_del_and_insert` flag in the logical update
    operator.
    
    This PR also adds a small `plan_serializer" tool which can be used to
    dump plans to disk and deserialize/verity them which is quite handy when
    debugging BWC issues in general and is a bit easier than dealing with
    the python+catch script.
    
    I've also added a couple test cases with specific serialized binary
    plans to ensure this fix in particular works.
    
    Upstream-Commit: https://github.com/duckdb/duckdb/commit/086a1b85e774031ea36183c6fa9d14d4a4ad20cb

--- external/duckdb/src/storage/serialization/serialize_logical_operator.cpp
+++ external/duckdb/src/storage/serialization/serialize_logical_operator.cpp
@@ -202,6 +202,9 @@ unique_ptr<LogicalOperator> LogicalOperator::Deserialize(Deserializer &deseriali
 	}
 	deserializer.Unset<LogicalOperatorType>();
 	result->children = std::move(children);
+	if (type == LogicalOperatorType::LOGICAL_UPDATE) {
+		LogicalUpdate::RewriteInPlaceUpdates(*result);
+	}
 	return result;
 }
 
