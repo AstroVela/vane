4f67e5bb9f25011a74ef907da13f21d96676d741
Laurens Kuiper <laurens@duckdblabs.com>
2026-04-22 11:03:11 +0200

    User-facing `enable_caching_operators` setting (duckdb/duckdb#22191)
    We already have this setting, just not user-facing, which this PR
    implements.
    
    It's enabled by default, can be disabled like so:
    ```sql
    SET enable_caching_operators=false;
    ```
    
    Disabling this can be beneficial when processing massive strings, for
    example, as demonstrated by the added test.
    
    Upstream-Commit: https://github.com/duckdb/duckdb/commit/62e7f0dddf75559584c862430c642efbdeb1c161

--- external/duckdb/src/main/config.cpp
+++ external/duckdb/src/main/config.cpp
@@ -127,6 +127,7 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_GLOBAL(DisabledOptimizersSetting),
     DUCKDB_SETTING_CALLBACK(DuckDBAPISetting),
     DUCKDB_SETTING(DynamicOrFilterThresholdSetting),
+    DUCKDB_LOCAL(EnableCachingOperatorsSetting),
     DUCKDB_SETTING_CALLBACK(EnableExternalAccessSetting),
     DUCKDB_SETTING_CALLBACK(EnableExternalFileCacheSetting),
     DUCKDB_SETTING(EnableFSSTVectorsSetting),
@@ -219,12 +220,12 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_SETTING(ZstdMinStringLengthSetting),
     FINAL_SETTING};
 
-static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 103),
+static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 104),
                                                      DUCKDB_SETTING_ALIAS("null_order", 43),
-                                                     DUCKDB_SETTING_ALIAS("profiling_output", 122),
-                                                     DUCKDB_SETTING_ALIAS("user", 137),
+                                                     DUCKDB_SETTING_ALIAS("profiling_output", 123),
+                                                     DUCKDB_SETTING_ALIAS("user", 138),
                                                      DUCKDB_SETTING_ALIAS("wal_autocheckpoint", 25),
-                                                     DUCKDB_SETTING_ALIAS("worker_threads", 136),
+                                                     DUCKDB_SETTING_ALIAS("worker_threads", 137),
                                                      FINAL_ALIAS};
 
 vector<ConfigurationOption> DBConfig::GetOptions() {
